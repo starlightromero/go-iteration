e60ae9c275887c2d91f8a10e7b3e7f32f6e0347e
Starlight Romero <starlightromero@protonmail.com>
2021-04-13 23:44:12 -0700

    [create] repeat iteration

---
+++ repeat_test.go
@@ -0,0 +1,43 @@
+package iteration
+
+import (
+    "testing"
+    "fmt"
+)
+
+func TestRepeat(t *testing.T) {
+
+	assertCorrectMessage := func(t testing.TB, repeated, expected string) {
+		t.Helper()
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	}
+
+	t.Run("repeating 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("repeating 0 times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("repeating -3 times", func(t *testing.T) {
+		repeated := Repeat("a", -3)
+		expected := ""
+		assertCorrectMessage(t, repeated, expected)
+	})
+}
+
+func ExampleRepeat() {
+    repeat := Repeat("a", 6)
+    fmt.Println(repeat)
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	Repeat("a", b.N)
+}
